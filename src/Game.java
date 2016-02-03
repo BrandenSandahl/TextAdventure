1ead90cc059e679871c0ec5b3972ff82d8178d13
BrandenSandahl <branden157@gmail.com>
2016-02-03 11:59:24 -0500

    Refactored code from Game.java into Player.java

--- src/Game.java
+++ src/Game.java
@@ -5,38 +5,20 @@ import java.util.Scanner;
  */
 public class Game {
 
+    //create a global scanner object
+    public static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
 
         System.out.println("Welcome friend");
-        System.out.println("What is your name?");
 
-        //create a scanner object
-        Scanner scanner = new Scanner(System.in);
+        //create new Player object
+        Player player = new Player();
 
-        String name = scanner.nextLine();        //read in user input
-        System.out.println("Welcome, " + name);
-
-        System.out.println("Choose your weapon [sword/mace]");
-        String weapon = scanner.nextLine();
-
-        //test the weapon
-        if (weapon.equalsIgnoreCase("sword") || weapon.equalsIgnoreCase("mace")) {
-            System.out.println("You have chosen a " + weapon + "!");
-        } else {
-            throw new Exception("You have chosen a " + weapon + ". Did you see that in the list of options? No. No you did not. Try again.");
-        }
-
-        //read in location
-        System.out.println("Choose your starting location [forest/tunnel]");
-        String startingLocation = scanner.nextLine();
-
-        if (startingLocation.equalsIgnoreCase("forest")) {
-            System.out.println("Entering forest...");
-        } else if (startingLocation.equalsIgnoreCase("tunnel")) {
-            System.out.println("Entering tunnel...");
-        } else {
-            throw new Exception("Invalid location");
-        }
+        //run methods on player
+        player.chooseName();
+        player.chooseWeapon();
+        player.chooseLocation();
 
     }
 
--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,4 @@
+
 /**
  * Created by branden on 2/3/16 at 11:37.
  */
@@ -5,7 +6,7 @@ public class Player {
 
 
     // Set up Vars
-    private String name, weapon, startingLocation;
+    private String name, weapon, location;
 
 
     // Constructors if used
@@ -18,4 +19,37 @@ public class Player {
 
     /** End of Getters and Setters */
 
+
+    public void chooseName() {
+        System.out.println("What is your name?");
+        this.name = Game.scanner.nextLine();        //read in user input
+        System.out.println("Welcome, " + name);
+
+    }
+
+    public void chooseWeapon() throws Exception {
+        System.out.println("Choose your weapon [sword/mace]");
+        this.weapon = Game.scanner.nextLine();
+
+        //test the weapon
+        if (weapon.equalsIgnoreCase("sword") || weapon.equalsIgnoreCase("mace")) {
+            System.out.println("You have chosen a " + weapon + "!");
+        } else {
+            throw new Exception("You have chosen a " + weapon + ". Did you see that in the list of options? No. No you did not. Try again.");
+        }
+    }
+
+    public void chooseLocation() throws Exception {
+        System.out.println("Choose your starting location [forest/tunnel]");
+         this.location = Game.scanner.nextLine();
+
+        if (location.equalsIgnoreCase("forest")) {
+            System.out.println("Entering forest...");
+        } else if (location.equalsIgnoreCase("tunnel")) {
+            System.out.println("Entering tunnel...");
+        } else {
+            throw new Exception("Invalid location");
+        }
+
+    }
 }
