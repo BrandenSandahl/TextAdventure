70283627e30ba65aa9016d4949932a0fa908a25d
BrandenSandahl <branden157@gmail.com>
2016-02-03 11:42:34 -0500

    Created new class

---
+++ src/Player.java
@@ -0,0 +1,21 @@
+/**
+ * Created by branden on 2/3/16 at 11:37.
+ */
+public class Player {
+
+
+    // Set up Vars
+    private String name, weapon, startingLocation;
+
+
+    // Constructors if used
+
+
+    /**
+    * Start Getters and Setters  End of Getters and Setters
+    */
+
+
+    /** End of Getters and Setters */
+
+}
